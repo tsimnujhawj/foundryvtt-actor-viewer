fd945badb55b502dcafad80b722561dd5b73d85e
Tsimnuj Hawj <tsimnuj.hawj@gmail.com>
2021-08-09 23:44:25 -0500

    removed unused app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const file = require("fs")
-
-const characterFile = file.readFileSync('./data/pcs/elrosro.json', "utf-8", (err, data)=>{
-    return data
-})
-const character = JSON.parse(characterFile)
-console.log(character.name)
